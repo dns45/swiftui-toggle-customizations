eeed4570c7c83653b34d0d234c9813a7d459c073
dns45 <dns45.one@gmail.com>
2020-10-12 00:42:58 +0200

    Create SwiftUI app

---
+++ ToggleCustomizations/ToggleCustomizationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleCustomizationsApp.swift
+//  ToggleCustomizations
+//
+//  Created by Teply, Daniel on 12/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct ToggleCustomizationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
